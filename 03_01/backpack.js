0ed14586d13b18d278383928f28ef17ec9babe41
Phuoc Thien Tran <donnyquickinc@gmail.com>
2023-06-26 20:20:01 +1000

    improvement of object properties relationship

--- 03_01/backpack.js
+++ 03_01/backpack.js
@@ -1,47 +1,48 @@
-// Define the backpack object
-var backpack = {
+// Use ES6 syntax to define the backpack object
+const backpack = {
   colors: ["red", "blue", "green"],
   sizes: ["small", "medium", "large"],
   width: "15cm",
   straps: 2,
   flatBackArea: true,
   opening: "top",
-  open: function () {
+  open() {
     document.getElementById("opening-statement").textContent =
       "The backpack is opened.";
   },
-  close: function () {
+  close() {
     document.getElementById("closing-statement").textContent =
       "The backpack is closed.";
   },
 };
 
-// Define the pen object
-var pen = {
+// Use ES6 syntax to define the pen object
+const pen = {
   color: "black",
   length: "15cm",
-  write: function () {
+  write() {
     document.getElementById("pen-statement").textContent =
       "The pen is writing.";
   },
 };
 
-// Fill in the backpack properties
-var backpackPropertiesList = document.getElementById("backpack-properties");
-var backpackProperties = [
-  "Colors: " + backpack.colors,
-  "Sizes: " + backpack.sizes,
-  "Width: " + backpack.width,
-  "Straps: " + backpack.straps,
-  "Flat Back Area: " + backpack.flatBackArea,
-  "Opening: " + backpack.opening,
+// Use ES6 template literals to fill in the backpack properties
+const backpackPropertiesList = document.getElementById("backpack-properties");
+const backpackProperties = [
+  `Colors: ${backpack.colors}`,
+  `Sizes: ${backpack.sizes}`,
+  `Width: ${backpack.width}`,
+  `Straps: ${backpack.straps}`,
+  `Flat Back Area: ${backpack.flatBackArea}`,
+  `Opening: ${backpack.opening}`,
 ];
 
-backpackProperties.forEach(function (property) {
-  var listItem = document.createElement("li");
+// Use a for...of loop to iterate over the backpack properties
+for (const property of backpackProperties) {
+  const listItem = document.createElement("li");
   listItem.textContent = property;
   backpackPropertiesList.appendChild(listItem);
-});
+}
 
 // Output the relationship statements
 backpack.open();
