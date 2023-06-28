edf9c9c3fecbfdb138219cf011a1b4687f20c9f9
Phuoc Thien Tran <donnyquickinc@gmail.com>
2023-06-28 11:01:06 +1000

    object introduction

--- 03_02/script.js
+++ 03_02/script.js
@@ -1,8 +1,26 @@
 /**
+ * Object pseudo-coding:
+
+let objectExample; //Declaration of objectExample
+objectExample = {
+  stringPropertyExample: "string property",
+  valuePropertyExample: 23,
+  arrayPropertyExample: {
+    name: "Backpack",
+    volume: 30,
+  },
+  booleanPropertyExample: false,
+  methodPropertyExample: function (methodParametersExample){
+    this.booleanPropertyExample = methodParametersExample;
+  },
+};
+ */
+
+/**
  * Create a Backpack object.
  */
 
-const backpack = {
+let backpack = {
   name: "Everyday Backpack",
   volume: 30,
   color: "grey",
@@ -12,9 +30,18 @@ const backpack = {
     right: 26,
   },
   lidOpen: false,
+  /**
+   * method determining if backpack is open or not
+   * @param {boolean} lidStatus 
+   */
   toggleLid: function (lidStatus) {
     this.lidOpen = lidStatus;
   },
+  /**
+   * method changing how long the strap lengths are
+   * @param {value} lengthLeft 
+   * @param {value} lengthRight 
+   */
   newStrapLength: function (lengthLeft, lengthRight) {
     this.strapLength.left = lengthLeft;
     this.strapLength.right = lengthRight;
