50f739ff5b745ec3ba092340ed511b1d902bb1c3
Phuoc Thien Tran <donnyquickinc@gmail.com>
2023-06-28 11:37:43 +1000

    const vs let in obj

--- 03_04/script.js
+++ 03_04/script.js
@@ -1,4 +1,14 @@
 /**
+ * Object's declarations:
+const: Use const when you want to declare a variable that won't be reassigned.
+The value assigned to a const variable cannot be changed, but properties of objects or 
+arrays assigned to a const variable can be modified.
+
+let: Use let when you want to declare a variable that can be reassigned.
+The value assigned to a let variable can be changed.
+ */
+
+/**
  * Create a Backpack object.
  */
 
