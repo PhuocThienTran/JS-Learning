79b0bde7271cab26cc2ea503be7dfee387e08d3a
Phuoc Thien Tran <donnyquickinc@gmail.com>
2023-06-28 14:01:36 +1000

    building basic obj with if/else

--- Practice/03_07/script.js
+++ Practice/03_07/script.js
@@ -7,3 +7,24 @@
  * - Find an object that has another object inside of it to create a nested object.
  * - Test your objects in the browser console by accessing the entire object and its specific properties.
  */
+
+
+let gameStation = {
+    name: "Nintendo Switch",
+    joystickNum: 2,
+    casing: true,
+    // modify between the properties' bool values to get different outcomes in the if condition
+    playConfig: {
+        tv: Boolean(),
+        portable: Boolean(true),
+    },
+};
+
+let playSituation = "playConfig";
+if(gameStation[playSituation].tv === true){
+    gameStation[playSituation].portable === false
+    console.log(gameStation[playSituation])
+} else {
+    gameStation[playSituation].portable === true
+    console.log(gameStation[playSituation])
+}
