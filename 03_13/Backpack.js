fd16e0efbf4b374ee5cf3de0ae56e69eca7eeca8
Phuoc Thien Tran <donnyquickinc@gmail.com>
2023-07-02 11:57:21 +1000

    global vars and explorations

--- 03_13/Backpack.js
+++ 03_13/Backpack.js
@@ -1,3 +1,10 @@
+/**
+ * 
+ "global objects" in JavaScript refers to a set of predefined objects that are built into the 
+JavaScript language. These objects provide a range of functionality and are available globally 
+in any JavaScript environment.
+ */
+
 class Backpack {
   constructor(
     name,
@@ -28,12 +35,30 @@ class Backpack {
     this.strapLength.right = lengthRight;
   }
   backpackAge() {
-    let now = new Date();
-    let acquired = new Date(this.dateAcquired);
+    let now = new Date(); // assign the Date global obj to the now variable, making it the Date obj
+    let acquired = new Date(this.dateAcquired); // assign acquired to the Date global obj with the argument of dateAcquired
     let elapsed = now - acquired; // elapsed time in milliseconds
     let daysSinceAcquired = Math.floor(elapsed / (1000 * 3600 * 24));
-    return daysSinceAcquired;
+    // A fun little if condition to test if daysSinceAcquired > 1000 then return its value else return current date
+    if (daysSinceAcquired > 1000){
+      return daysSinceAcquired;
+    } else {
+      return now;
+    }
   }
 }
 
 export default Backpack;
+
+/**
+Argument vs. Parameter:
+Parameters are the variables defined in the function declaration. They act as placeholders.
+Arguments are the actual values passed to the function when it is called. They provide the actual values to be used within the function.
+
+Parameters:
+function greet(name) {
+  console.log("Hello, " + name + "!");
+}
+Arguments:
+new Date(this.dateAcquired);
+ */
