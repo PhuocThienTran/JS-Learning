7ede274f1c5bf47c5826663973d7e9ce04f0139e
Phuoc Thien Tran <donnyquickinc@gmail.com>
2023-07-04 16:28:28 +1000

    add backpack class and obj for querySelector prac

---
+++ Practice/05_04/backpackObj.js
@@ -0,0 +1,36 @@
+/**
+ * Note: This file is intentionally empty.
+ * You can use it to test your skills at traversing the DOM using JavaScript.
+ */
+
+import Backpack from "./Backpack.js";
+
+const backpacks = [
+    new Backpack(
+      "pack01",
+      "everyday",
+      "Everyday Backpack",
+      "30l",
+      "grey",
+      "684",
+      "15",
+      "26 inches",
+      "26 inches",
+      "closed"
+    ),
+    new Backpack(
+      "pack02",
+      "frog",
+      "Frog Backpack",
+      "8l",
+      "green",
+      "369",
+      "3",
+      "10 inches",
+      "10 inches",
+      "closed"
+    ),
+  ];
+  
+  export default backpacks;
+  
