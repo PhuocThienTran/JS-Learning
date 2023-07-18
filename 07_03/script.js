97bc76fa784f2ea5101ce2530d78600943812c22
Phuoc Thien Tran <donnyquickinc@gmail.com>
2023-07-18 14:38:24 +1000

    explain what the examples are about

--- 07_03/script.js
+++ 07_03/script.js
@@ -7,11 +7,13 @@ let backpackContents = ["piggy", "headlamp", "pen"];
 
 console.log(backpackContents);
 
+// This generates all the array's items per list item in the console log
 // backpackContents.forEach(function (item) {
 //   item = `<li>${item}</li>`;
 //   console.log(item);
 // });
 
+// This checks if an item's length in the array is >= 5 then ouput the item
 // let longItems = backpackContents.find(function (item) {
 //   if (item.length >= 5) {
 //     return item;
