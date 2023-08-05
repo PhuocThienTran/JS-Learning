d23f48c1f05720bffaa5bc240e1b147970cde4a0
Phuoc Thien Tran <donnyquickinc@gmail.com>
2023-08-05 19:33:30 +1000

    practice 8.6

--- Practice/08_06/script.js
+++ Practice/08_06/script.js
@@ -1,7 +1,36 @@
 /**
  * Practice: Pass values between functions
  *
- * - Create two functions
- * - Main function creates article element with data from object
- * - Helper function creates.
+ * - Create a basic function declaration
+ * - Make a change to an element in the DOM (querySelector, adding a class, and so on)
+ * - Call the function declaration
+ * - Create a basic function expression
+ * - Repeat with an arrow function
  */
+
+// Basic function declaration
+function changeElementText() {
+  const element = document.querySelector("#myDiv");
+  element.textContent = "Text has been changed!"; // change This is a div element. to Text has been changed.
+}
+
+// Calling the function declaration
+changeElementText();
+
+// Basic function expression
+const changeElementColor = function () {
+  const element = document.querySelector("#myDiv");
+  element.style.color = "blue";
+};
+
+// Calling the function expression
+changeElementColor();
+
+// Arrow function
+const addClassToElement = () => {
+  const element = document.querySelector("#myDiv");
+  element.classList.add("highlight"); // add a class named highlight into the div with id myDiv
+};
+
+// Calling the arrow function
+addClassToElement();
